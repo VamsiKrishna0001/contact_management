fe7f851647e0c4b2b0ac5a7bea1c5b8e713a8543
Nitin Gupta <iamvamsi999@gmail.com>
2023-05-03 17:09:34 +0530

    add btn

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Container, Grid, Paper, TextField, Typography } from '@mui/material';
+import { Button, Container, Dialog, DialogActions, DialogContent, DialogTitle, Grid, Paper, TextField, Typography } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 const useStyles = makeStyles((theme) => ({
@@ -36,14 +36,51 @@ function ContactListPage() {
   const classes = useStyles();
   const [searchTerm, setSearchTerm] = useState('');
   const [contacts, setContacts] = useState(CONTACTS);
+  const [addContactOpen, setAddContactOpen] = useState(false);
+  const [newContactName, setNewContactName] = useState('');
+  const [newContactEmail, setNewContactEmail] = useState('');
+  const [newContactPhone, setNewContactPhone] = useState('');
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
   const handleDelete = (id) => {
-    console.log("id.. ", id);
     setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
+  };
+
+  const handleAddContactOpen = () => {
+    setAddContactOpen(true);
+  };
+
+  const handleAddContactClose = () => {
+    setAddContactOpen(false);
+    setNewContactName('');
+    setNewContactEmail('');
+    setNewContactPhone('');
+  };
+
+  const handleNewContactNameChange = (event) => {
+    setNewContactName(event.target.value);
+  };
+
+  const handleNewContactEmailChange = (event) => {
+    setNewContactEmail(event.target.value);
+  };
+
+  const handleNewContactPhoneChange = (event) => {
+    setNewContactPhone(event.target.value);
+  };
+
+  const handleAddContact = () => {
+    const newContact = {
+      id: contacts.length + 1,
+      name: newContactName,
+      email: newContactEmail,
+      phone: newContactPhone,
+    };
+    setContacts([...contacts, newContact]);
+    handleAddContactClose();
   };
 
   const filteredContacts = contacts.filter((contact) =>
@@ -64,22 +101,60 @@ function ContactListPage() {
           onChange={handleSearchChange}
           className={classes.search}
         />
+        <Button variant="contained" color="primary" onClick
+          ={(event) => handleAddContactOpen()}>
+          Add Contact
+        </Button>
       </Paper>
-      <Grid container spacing={2}>
-        {filteredContacts.map((contact) => (
-          <Grid item xs={12} key={contact.id}>
-            <Paper className={classes.paper}>
-              <Typography variant="h6">{contact.name}</Typography>
-              <Typography variant="subtitle1">{contact.email}</Typography>
-              <Typography variant="subtitle1">{contact.phone}</Typography>
-              <DeleteIcon
-                className={classes.deleteIcon}
-                onClick={() => handleDelete(contact.id)}
-              />
-            </Paper>
-          </Grid>
-        ))}
-      </Grid>
+      {filteredContacts.map((contact) => (
+        <Grid item xs={12} key={contact.id}>
+          <Paper className={classes.paper}>
+            <Typography variant="h6">{contact.name}</Typography>
+            <Typography variant="subtitle1">{contact.email}</Typography>
+            <Typography variant="subtitle1">{contact.phone}</Typography>
+            <DeleteIcon
+              className={classes.deleteIcon}
+              onClick={() => handleDelete(contact.id)}
+            />
+          </Paper>
+        </Grid>
+      ))}
+      <Dialog open={addContactOpen} onClose={handleAddContactClose}>
+        <DialogTitle>Add New Contact</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Name"
+            type="text"
+            fullWidth
+            value={newContactName}
+            onChange={handleNewContactNameChange}
+          />
+          <TextField
+            margin="dense"
+            label="Email"
+            type="email"
+            fullWidth
+            value={newContactEmail}
+            onChange={handleNewContactEmailChange}
+          />
+          <TextField
+            margin="dense"
+            label="Phone"
+            type="tel"
+            fullWidth
+            value={newContactPhone}
+            onChange={handleNewContactPhoneChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleAddContactClose}>Cancel</Button>
+          <Button onClick={handleAddContact} color="primary">
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 }
